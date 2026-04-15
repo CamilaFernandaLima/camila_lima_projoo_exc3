a4998e3faae1017a7ad986e12237b856bd1b121b
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2026-04-15 16:41:01 -0300

    criação da interface bebiba e das bebidas base.

---
+++ exercicio_3/exercicio_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// 1. interface comum para as bebidas
+public interface IBebida
+{
+    string GetDescricao();
+    double GetPreco();
+}
+
+// bebidas base (ainda sem complementos)
+public class CafeExpresso : IBebida
+{
+    public string GetDescricao() => "Café Expresso";
+    public double GetPreco() => 6.0;
+}
+public class Capuccino : IBebida
+{
+    public string GetDescricao() => "Capuccino";
+    public double GetPreco() => 8.0;
+}
+public class Cha : IBebida
+{
+    public string GetDescricao() => "Chá";
+    public double GetPreco() => 4.0;
+}
